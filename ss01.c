e45027cc9d8352048704c3b96b87e267c9f89f00
itsdevkr <38876728+itsdevkr@users.noreply.github.com>
2019-08-17 17:29:12 +0530

    Create ss01.c

---
+++ ss01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(j<=i)
+				printf("*");
+			else
+				printf(" ");	
+		}
+		printf("\n");
+	}
+	return 0;
+}
