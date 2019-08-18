325f19f244772211c48e4d053d814292fa0032c2
itsdevkr <38876728+itsdevkr@users.noreply.github.com>
2019-08-18 09:10:54 +0530

    Create ss11.c

---
+++ ss11.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(j<=i)
+			printf("*");
+			else
+			printf(" ");
+		}
+		printf("\n");
+	}
+	for(i=1;i<n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(j>n-i)
+			printf(" ");
+			else
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
